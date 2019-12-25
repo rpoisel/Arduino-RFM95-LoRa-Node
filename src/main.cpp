7cac9bab9219bb85ae8a24febb2ece92f8227144
Rainer Poisel <rainer.poisel@gmail.com>
2019-12-25 15:54:41 +0100

    Using LoRa Library instead of RadioHead

--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,45 @@
-// rf95_client.pde
-// -*- mode: C++ -*-
-// Example sketch showing how to create a simple messageing client
-// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
-// reliability, so you should only use RH_RF95 if you do not need the higher
-// level messaging abilities.
-// It is designed to work with the other example rf95_server
-// Tested with Anarduino MiniWirelessLoRa, Rocket Scream Mini Ultra Pro with
-// the RFM95W, Adafruit Feather M0 with RFM95
+#include <LoRa.h>
 
-#include <RH_RF95.h>
+constexpr int const LORA_SS = 10;
+constexpr int const LORA_RESET = 9;
+constexpr int const LORA_DIO0 = 7;
 
-// Singleton instance of the radio driver
-// RH_RF95 rf95;
-static RH_RF95 rf95(10, 7); // Rocket Scream Mini Ultra Pro with the RFM95W
-// RH_RF95 rf95(8, 3); // Adafruit Feather M0 with RFM95
-
-// Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
-//#define Serial SerialUSB
+static size_t counter = 0;
 
 void setup()
 {
-  // Rocket Scream Mini Ultra Pro with the RFM95W only:
-  // Ensure serial flash is not interfering with radio communication on SPI bus
-  //  pinMode(4, OUTPUT);
-  //  digitalWrite(4, HIGH);
-
   Serial.begin(9600);
   while (!Serial)
-    ; // Wait for serial port to be available
-  if (!rf95.init())
-  {
-    Serial.println("init failed");
-  }
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+    ;
 
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-  // you can set transmitter powers from 5 to 23 dBm:
-  //  driver.setTxPower(23, false);
-  // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
-  // transmitter RFO pins and not the PA_BOOST pins
-  // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true.
-  // Failure to do that will result in extremely low transmit powers.
-  //  driver.setTxPower(14, true);
-  if (!rf95.setFrequency(868.0))
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(868E6))
   {
-    Serial.println("setFrequency failed");
+    Serial.println("Starting LoRa failed!");
     while (1)
       ;
   }
-  rf95.setSpreadingFactor(7);
-  rf95.setPreambleLength(8);
-  rf95.setSignalBandwidth(125000);
-  rf95.setPayloadCRC(false);
-  rf95.setCodingRate4(5);
+  LoRa.setPins(LORA_SS, LORA_RESET, LORA_DIO0);
+  LoRa.setSpreadingFactor(7);
+  LoRa.setPreambleLength(8);
+  LoRa.setSignalBandwidth(125000);
+  LoRa.disableCrc();
+  LoRa.setCodingRate4(5);
 }
 
 void loop()
 {
-  Serial.println("Sending to rf95_server");
-  // Send a message to rf95_server
-  uint8_t data[] = "Hello World!";
-  rf95.send(data, sizeof(data));
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
 
-  rf95.waitPacketSent();
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
 
-  if (rf95.waitAvailableTimeout(3000))
-  {
-    // Should be a reply message for us now
-    if (rf95.recv(buf, &len))
-    {
-      Serial.print("got reply: ");
-      Serial.println(reinterpret_cast<char const*>(&buf[0]));
-    }
-    else
-    {
-      Serial.println("recv failed");
-    }
-  }
-  else
-  {
-    Serial.println("No reply, is rf95_server running?");
-  }
+  counter++;
+
   delay(1000);
 }
