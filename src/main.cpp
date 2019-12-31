474091cd5a85711bad9c1681acb31ab60c3aaccc
Rainer Poisel <rainer.poisel@gmail.com>
2019-12-31 23:31:45 +0100

    Encrypting messages using AES256

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
+#include <AES.h>
 #include <LoRa.h>
 
 constexpr int const LORA_SS = 10;
 constexpr int const LORA_RESET = 9;
 constexpr int const LORA_DIO0 = 7;
+constexpr uint8_t const AES_KEY[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                                     0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
 
 static size_t counter = 0;
+static AESTiny256 aes;
 
 void setup()
 {
@@ -13,6 +17,7 @@ void setup()
     ;
 
   Serial.println("LoRa Sender");
+  aes.setKey(&AES_KEY[0], aes.keySize());
 
   if (!LoRa.begin(868E6))
   {
@@ -30,14 +35,22 @@ void setup()
 
 void loop()
 {
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
+  uint8_t buffer[16]{'\0'};
 
-  // send packet
-  LoRa.beginPacket();
-  LoRa.print("hello ");
-  LoRa.print(counter);
-  LoRa.endPacket();
+  String msg("Hello ");
+  msg += counter;
+
+  if (msg.length() <= sizeof(buffer) - 1)
+  {
+    aes.encryptBlock(buffer, reinterpret_cast<uint8_t const*>(msg.c_str()));
+
+    Serial.print("Sending packet: ");
+    Serial.println(counter);
+
+    LoRa.beginPacket();
+    LoRa.write(&buffer[0], sizeof(buffer));
+    LoRa.endPacket();
+  }
 
   counter++;
 
