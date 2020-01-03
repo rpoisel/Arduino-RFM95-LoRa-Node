30ec2519a607190cfc5d80ab073160af09bf715e
Rainer Poisel <rainer.poisel@gmail.com>
2020-01-03 23:56:21 +0100

    Preparing nonce based authentication

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "datatypes.h"
+
 #include <AES.h>
 #include <LoRa.h>
 
@@ -7,8 +9,9 @@ constexpr int const LORA_DIO0 = 7;
 constexpr uint8_t const AES_KEY[32] = {
     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
     0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f};
+constexpr LoRaNodeID const NODE_ID = 0x0001;
 
-static size_t counter = 0;
+static uint32_t counter = 0;
 static AESTiny256 aes;
 
 void setup()
@@ -34,32 +37,20 @@ void setup()
   LoRa.setCodingRate4(5);
 }
 
-constexpr size_t const AES_MSG_LEN = 16;
-
 void loop()
 {
-  uint8_t encrypted[AES_MSG_LEN]{'\0'};
+  LoRaPayload payload(NODE_ID);
+  payload.cmd = SensorData;
+  payload.sensordata.value = counter;
+  uint8_t encrypted[sizeof(payload)];
+  aes.encryptBlock(encrypted, reinterpret_cast<uint8_t const*>(&payload));
 
-  String msg("RPOxHello ");
-  msg += counter;
-  msg.setCharAt(3, msg.length() + sizeof('\0') - 4 /* RPOx */);
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
 
-  for (size_t cnt = msg.length() + sizeof('\0'); cnt < AES_MSG_LEN; cnt++)
-  {
-    msg.setCharAt(cnt, LoRa.random());
-  }
-
-  if (msg.length() <= sizeof(encrypted) - 1)
-  {
-    aes.encryptBlock(encrypted, reinterpret_cast<uint8_t const*>(msg.c_str()));
-
-    Serial.print("Sending packet: ");
-    Serial.println(counter);
-
-    LoRa.beginPacket();
-    LoRa.write(&encrypted[0], sizeof(encrypted));
-    LoRa.endPacket();
-  }
+  LoRa.beginPacket();
+  LoRa.write(&encrypted[0], sizeof(encrypted));
+  LoRa.endPacket();
 
   counter++;
 
